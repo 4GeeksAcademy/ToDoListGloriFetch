5ca56de21dba36a48a2387a0e4a908b48add84ee
Glori <128950766+GlorianaZelaya@users.noreply.github.com>
2023-07-22 03:41:34 +0000

    commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -33,6 +33,30 @@ const Home = () => {
 		}
 		fetch(todoApi).then(result => response => response.json()).then(data => console.log(data)).catch(error => error)
 	}
+	function addTodo(todos) {
+		fetch(
+			"https://fake-todo-list-52f9a4ed80ce.herokuapp.com/todos/user/GlorianaZelaya",
+			{
+				method: "PUT",
+				body: JSON.stringify(todos),
+				headers: {
+					"Content-Type": "application/json",
+				},
+			}
+		)
+			.then((resp) => {
+				console.log(resp.ok);
+				console.log(resp.status);
+				console.log(resp.text());
+				return resp.json();
+			})
+			.then((data) => {
+				console.log(data);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
 
 	//Delete b
 	function deleteTask(todos) {
